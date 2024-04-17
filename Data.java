461e00eb4bdfa4d47da0c75bccc69bcc2e86e580
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2024-04-17 12:00:13 +0200

    DataClass

--- Data.java
+++ Data.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public interface Data {
 
+    ArrayList<User> readUserData(String path);
 
+ void saveUserData (List<User> users);
+ void saveWatchedMovie (List<Movie> watched);
+ void saveSavedMovie (List<Movie> saved);
+ String[] readMovieData (String path, int length);
 
 }
