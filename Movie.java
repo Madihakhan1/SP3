fb4d49b6204ec6af043ed02858d8517b806cebf4
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2024-04-17 10:50:41 +0200

    MovieClassUpdated

--- Movie.java
+++ Movie.java
@@ -1,6 +1,46 @@
 public class Movie {
 
-    //Titlen på film, årstal, genre, rating
-    // lav en toString.
+private String title;
+private int publicationYear;
+private String genre;
+private double rating;
+
+
+
+public Movie(String title, int year, String genre, double rating){
+
+    this.title = title;
+    this.publicationYear = publicationYear;
+    this.genre = genre;
+    this.rating = rating;
+
+}
+
+public String getTitle(){
+
+    return title;
+}
+
+public int getPublicationYear(){
+    return publicationYear;
+}
+
+public String getGenre(){
+    return genre;
+}
+
+public double getRating(){
+
+    return rating;
+}
+
+@Override
+public String toString(){
+    return "Title: " + title +
+            "\n" + "Year: " + publicationYear +
+            "\n" + "Genre: " + genre +
+            "\n" + "Rating: " + rating;
+
+}
 
 }
