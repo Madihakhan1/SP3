026f858dda820aea94753abf8071ffd53689306f
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2024-04-17 10:51:19 +0200

    UserCLassUpdated

--- User.java
+++ User.java
@@ -1,5 +1,14 @@
 public class User {
 
+    private String userName;
+    private String userPassword;
+
+
+    public User(String userName, String userPassword){
+      this.userName;
+      this.userPassword
+
+    }
     //navn og kode på brugeren.
     //Eks. vis brugernes sete liste og gemte liste.
 
