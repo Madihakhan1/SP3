3cca1b2b68dda2fdf62c62ba25865f5fd115a932
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2024-04-18 13:40:12 +0200

    Updated StartMenu

--- StartMenu.java
+++ StartMenu.java
@@ -1,12 +1,37 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class StartMenu {
 
+    private String userName;
+    private String userPassword;
+    private User currentUser;
+    protected List<User> users = new LinkedList<>();
+    private TextUI ui = new TextUI();
+    private FileIO io = new FileIO();
 
-    public void createUser(){
+
+public void createUser(){
+
+    ui.displayMessage("Create a user");
+
+    this.userName = ui.getInput("Please type a username:");
+    this.userPassword = ui.getInput("Please type a password");
+
+    User createdUser = new User(userName, userPassword);
+    this.users.add(createdUser);
+    io.saveUserData(users);
 
 
-    }
+}
+
 
 public void login(){
+
+    ui.displayMessage("Please ");
+
+
 
 
 }
