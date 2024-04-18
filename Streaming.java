2aa1c96d74f5302aca5da085d4f076b1e1576e85
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2024-04-18 13:40:31 +0200

    UpdatedStreaming

--- Streaming.java
+++ Streaming.java
@@ -5,12 +5,52 @@ import java.util.List;
 public class Streaming {
 
     private List<Movie> allMovies = new ArrayList<Movie>();
+    private ArrayList<String> listOfOptions;
 
-    private TextUI ui;
-    private FileIO io;
+
+    private TextUI ui = new TextUI();
+    private FileIO io = new FileIO();
+
+    StartMenu startMenu = new StartMenu();
 
 
 public void displayStartMenu (){
+ui.displayMessage("Welcome to ChillFlex, do you want to create a user or login?");
+
+String options = "";
+boolean choosing = true;
+//while (choosing){
+
+    options = ui.getInput("Choose an option: \n Option 1: create a user \n option 2: login");
+
+
+    switch(options) {
+        case "1":
+            startMenu.createUser();
+            startMenu.login();
+
+
+            break;
+        case "2":
+
+            break;
+
+        case "3":
+
+            break;
+
+        case "4":
+
+            break;
+
+
+        default:
+            // code block
+    }
+
+
+//}
+
 
 }
 
